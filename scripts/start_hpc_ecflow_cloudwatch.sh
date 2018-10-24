842d3ecd29d071c7bfc2b774bac04c599adda8d4
Marcel Wokke <marcel.wokke@meteogroup.com>
2018-10-24 12:23:05 +0200

    modified ip

--- scripts/start_hpc_ecflow_cloudwatch.sh
+++ scripts/start_hpc_ecflow_cloudwatch.sh
@@ -2,6 +2,6 @@
 
 maindir=/home/vagrant/Documents/hpc-ecflow-cloudwatch/
 
-uwsgi --http 10.31.99.60:8000 --wsgi-file ${maindir}app.py --callable app
+uwsgi --http 10.31.41.24:8000 --wsgi-file ${maindir}app.py --callable app
 
 
