3d63d95b55e61af5ff25dcbe81620c45fe3e62d3
rahulinux <loginrahul90@gmail.com>
2019-06-14 12:09:54 +0200

    feat: connect ecflow as local

---
+++ ecflow_cloudwatch/ecflow_client_utils.py
@@ -0,0 +1,25 @@
+import os
+import ecflow
+from .external import run_subprocess
+
+
+class GetEcflowStats(object):
+
+    def fetch_ecflow_stats(self):
+        # Create the client. This will read the default environment variables
+        ecflow_host = os.getenv('ECF_HOST', "localhost")
+        ecflow_port = os.getenv('ECF_PORT', "3143")
+        ci = ecflow.Client(ecflow_host, ecflow_port)
+
+        # Get the node tree suite definition as stored in the server
+        # The definition is retrieved and stored on the variable 'ci'
+        ci.sync_local()
+
+        # access the definition retrieved from the server
+        server_defs = ci.get_defs()
+
+        if server_defs == None :
+            print "The server has no definition"
+            exit(1)
+        return server_defs
+
